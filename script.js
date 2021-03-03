1b056c7c0c0cf19f68f71c50245d4f4eb6479faf
Erik Luna <erikluna30@yahoo.com>
2021-03-02 21:47:02 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+//navbar
+const menu = document.querySelector(".menu");
+const navbar = document.querySelector(".navbar");
+
+menu.addEventListener('click', () => {
+    navbar.classList.toggle("change")
+    menu.classList.toggle("change")
+})
+//end of navbar
+
+// Initialize swiper
+var swiper = new Swiper('.swiper-container', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+      rotate: 70,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+  });
